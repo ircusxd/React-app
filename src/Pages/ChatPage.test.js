424397cfc30dc3ccbd955a3ef48133a2182e005d
Ircus <130798991+ircusxd@users.noreply.github.com>
2025-01-30 19:32:24 +0100

    Delete src/Pages/ChatPage.test.js

--- src/Pages/ChatPage.test.js
+++
@@ -1,8 +0,0 @@
-// src/Pages/ChatPage.test.js
-import { render, screen } from '@testing-library/react';
-import ChatPage from './ChatPage';
-
-test('debe renderizar la página ChatPage', () => {
-  render(<ChatPage />);
-  expect(screen.getByText('Texto esperado en la página ChatPage')).toBeInTheDocument();
-});
