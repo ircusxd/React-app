1f6a9a2078ab0fe00994cca3a4b040d148b9518c
Ircus <130798991+ircusxd@users.noreply.github.com>
2025-01-30 19:32:11 +0100

    Delete src/Components/Chat.test.js

--- src/Components/Chat.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Chat from './Chat';
-
-test('debe renderizar el componente Chat', () => {
-  render(<Chat />);
-  expect(screen.getByText('Texto esperado en el componente Chat')).toBeInTheDocument();
-});
